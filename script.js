e85f00d22861048fb7ca2d0b8d2c31cabb53c1d4
KvinCodes <maur.av007@gmail.com>
2024-09-25 12:37:02 -0600

    Nuevos cambios

--- script.js
+++ script.js
@@ -57,14 +57,15 @@ cancion.addEventListener("loadedmetadata", function() {
     progreso.max = cancion.duration;
     progreso.value = cancion.currentTime;
 
-    window.addEventListener("load", function() {
-        reproducirCancion();
-    });
 
     // Actualiza la duración total
     const duracionMinutos = Math.floor(cancion.duration / 60);
     const duracionSegundos = Math.floor(cancion.duration % 60);
     duracionTotal.innerHTML = `${duracionMinutos}:${duracionSegundos < 10 ? '0' : ''}${duracionSegundos}`; // Formato mm:ss
+});
+
+window.addEventListener("load", function() {
+    reproducirCancion();
 });
 
 botonReproducirPausar.addEventListener("click", reproducirPausar);
